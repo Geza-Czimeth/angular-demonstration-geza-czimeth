a01f8b764c74ac28a7950b67be305ce35088605b
Geza Czimeth <gezaczimeth@gmail.com>
2023-03-19 11:31:10 +0100

    Listing cars added

--- src/app/components/carlist/carlist.component.ts
+++ src/app/components/carlist/carlist.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Car} from "../shared/car.model";
 
 @Component({
   selector: 'app-carlist',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class CarlistComponent {
 
+  cars: Car[] = [
+    {
+      name: 'Amber Fantasy',
+      type: 'Sportcar',
+      speed: 12,
+      imagePath: 'https://en.vnmod.net/wp-content/uploads/2022/02/020220221643773546.png'
+    },
+    {
+      name: 'Retro thunder',
+      type: 'Classic',
+      speed: 11,
+      imagePath: 'https://i.pinimg.com/originals/d0/0f/df/d00fdf570efd37d5a93e04d37722ad91.jpg'
+    }
+  ];
 }
